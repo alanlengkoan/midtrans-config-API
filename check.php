c1f56f54e59827576c4b009918f197fdd3f8cee0
Alan Lengkoan <alanlengkoan15@gmail.com>
2020-05-18 16:59:48 +0800

    tambah halaman check

--- check.php
+++ check.php
@@ -18,3 +18,35 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 $data = file_get_contents('php://input');
 // untuk mengubah extension file
 header('Content-Type: application/json');
+
+$charge_result = chargeAPI($url, $client_key, $data);
+
+http_response_code($charge_result['http_code']);
+
+echo $charge_result['body'];
+
+function chargeAPI($url, $client_key, $data)
+{
+    $ch = curl_init();
+    $curl_options = array(
+        CURLOPT_URL            => $url,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_POST           => 1,
+        CURLOPT_HEADER         => 0,
+        CURLOPT_HTTPHEADER     => array(
+            'Content-Type: application/json',
+            'Accept: application/json',
+            'Authorization: Basic ' . base64_encode($client_key . ':')
+        ),
+        CURLOPT_POSTFIELDS    => $data
+    );
+
+    curl_setopt_array($ch, $curl_options);
+    
+    $result = array(
+        'body' => curl_exec($ch),
+        'http_code' => curl_getinfo($ch, CURLINFO_HTTP_CODE),
+    );
+    
+    return $result;
+}
