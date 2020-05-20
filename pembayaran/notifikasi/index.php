172420fb77093aaa8dd079ef36dc65482e464a02
Alan Lengkoan <alanlengkoan15@gmail.com>
2020-05-20 10:30:30 +0800

    untuk sementara

--- pembayaran/notifikasi/index.php
+++ pembayaran/notifikasi/index.php
@@ -2,28 +2,10 @@
 $client_key    = 'SB-Mid-client-C_B1t7C5Q0B47okb';
 $if_production = false;
 $url           = ($if_production == true) ? 'https://app.midtrans.com/snap/v1/transactions' : 'https://app.sandbox.midtrans.com/snap/v1/transactions';
-
-if (!strpos($_SERVER['REQUEST_URI'], '/change')) {
-    http_response_code(404);
-    echo 'salah path, silahkan dicek apabila sudah benar berada di /change!';
-    exit();
-}
-
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(404);
-    echo 'halaman tidak ditemukan!';
-    exit();
-}
 // untuk mengambil data
 $data = file_get_contents('php://input');
 // untuk mengubah extension file
 header('Content-Type: application/json');
-
-$charge_result = chargeAPI($url, $client_key, $data);
-
-http_response_code($charge_result['http_code']);
-
-echo $charge_result['body'];
 
 function chargeAPI($url, $client_key, $data)
 {
@@ -50,3 +32,7 @@ function chargeAPI($url, $client_key, $data)
 
     return $result;
 }
+
+$charge_result = chargeAPI($url, $client_key, $data);
+
+print_r($charge_result);
